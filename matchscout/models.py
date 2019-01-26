be12556f8ece4f825e11d08c2db289ddba845542
kindsethe <44417609+kindsethe@users.noreply.github.com>
2019-01-26 13:31:10 -0500

    updated matchscout
    added fields, +/- signs

--- matchscout/models.py
+++ matchscout/models.py
@@ -2,13 +2,42 @@ from django.db import models
 
 # Create your models here.
 class matchscout(models.Model):
+	# pre-match info
 	name = models.CharField(max_length = 50)
 
-	team_num = models.CharField(max_length = 4)
+	team_num = models.IntegerField()
+
+	match_num = models.IntegerField()
 
 	alliance_colors = (('Red','Red'),('Blue','Blue'))
 	alliance = models.CharField(max_length = 4, choices = alliance_colors, default = 'Red')
 
-	score = models.IntegerField(blank = True)
+	starting_level = ((1, 'Level 1'), (2, 'Level 2'))
+	starting = models.IntegerField(choices = starting_level, default = 'Level 1')
+	# during match info
 
-	comments = models.CharField(max_length = 250)
+	s_hatches_1 = models.IntegerField(default = 0)
+	s_hatches_2 = models.IntegerField(default = 0)
+	s_hatches_3 = models.IntegerField(default = 0)
+
+	s_cargo_1 = models.IntegerField(default = 0)
+	s_cargo_2 = models.IntegerField(default = 0)
+	s_cargo_3 = models.IntegerField(default = 0)	
+
+	t_hatches_1 = models.IntegerField(default = 0)
+	t_hatches_2 = models.IntegerField(default = 0)
+	t_hatches_3 = models.IntegerField(default = 0)
+
+	t_cargo_1 = models.IntegerField(default = 0)
+	t_cargo_2 = models.IntegerField(default = 0)
+	t_cargo_3 = models.IntegerField(default = 0)
+
+	# post-match info
+	ending_level = ((0, 'None'), (1, 'Level 1'), (2, 'Level 2'), (3, 'Level 3'))
+	ending = models.IntegerField(choices = ending_level, default = 'None')
+
+	
+
+	#score = models.IntegerField(blank = True)
+
+	comments = models.TextField()
