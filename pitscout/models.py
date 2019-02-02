a12717691e353ae61d72cf577850b1260f2d541a
kindsethe <44417609+kindsethe@users.noreply.github.com>
2019-02-02 17:59:09 -0500

    fixed pitscout issue

--- pitscout/models.py
+++ pitscout/models.py
@@ -11,16 +11,16 @@ class pitscout(models.Model):
 	platform_start_yn = (('yes','yes'),('no','no'))
 	platform_start = models.CharField(max_length = 5, choices = platform_start_yn, default = 'yes')
 
-	platform_end_level = ((1,'Level 1'),(2,'Level 2'),(3,'Level 3'),(0, 'None'))
+	platform_end_level = (('1','Level 1'),('2','Level 2'),('3','Level 3'),('0', 'None'))
 	platform_end = models.CharField(max_length = 10, choices = platform_end_level, default = 'None')
 
 	climb_time = models.IntegerField()
 
 	disk_options = (('ground','Ground'),('h_player','Human Player'),('none','None'))
-	disk = models.CharField(max_length = 5, choices = disk_options, default = 'None')
+	disk = models.CharField(max_length = 10, choices = disk_options, default = 'None')
 
 	ball_options = (('ground','Ground'),('h_player','Human Player'),('none','None'))
-	ball = models.CharField(max_length = 5, choices = ball_options, default = 'None')
+	ball = models.CharField(max_length = 10, choices = ball_options, default = 'None')
 
 	auton_yn = (('yes','yes'),('no','no'))
 	auton = models.CharField(max_length = 5, choices = auton_yn, default = 'yes')
@@ -39,4 +39,4 @@ class pitscout(models.Model):
 
 	name = models.CharField(max_length = 50)
 
-	robot_photo = models.FileField(upload_to='robot_photo/',max_length=1000,default='')
+	#robot_photo = models.FileField(upload_to='robot_photo/',max_length=1000,default='')
--- pitscout/views.py
+++ pitscout/views.py
@@ -4,10 +4,12 @@ from .forms import pitscout_form
 from .models import pitscout
 
 # Create your views here.
+
 def pitscout(request):
+
 	if request.method == 'POST':
 		form = pitscout_form(request.POST)
-    
+		
 		if form.is_valid():
 			obj = form.save(commit=False)
 			obj.save()
